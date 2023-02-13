8f2c458f7012ba4a27f2c0e069e4e950730bc813
danielsf <scott.daniel@alleninstitute.org>
2023-02-13 13:30:53 -0800

    test very sparse matrix

--- cell_type_mapping_tree/tests/utils/test_sparse_utils.py
+++ cell_type_mapping_tree/tests/utils/test_sparse_utils.py
@@ -124,3 +124,33 @@ def test_load_csr_chunk():
                 data[r0:r1, c0:c1])
 
     _clean_up(tmp_path)
+
+
+def test_load_csr_chunk_very_sparse():
+    tmp_path = tempfile.mkdtemp(suffix='.zarr')
+
+    data = np.zeros((20, 20), dtype=int)
+    data[7, 11] = 1
+
+    csr = scipy_sparse.csr_matrix(data)
+    ann = anndata.AnnData(csr)
+    ann.write_zarr(tmp_path)
+
+    with zarr.open(tmp_path, 'r') as written_zarr:
+        for subset, expected_sum in zip([((1, 15), (6, 13)),
+                                         ((15, 19), (3, 14))],
+                                        [1, 0]):
+
+            expected = data[subset[0][0]:subset[0][1],
+                            subset[1][0]:subset[1][1]]
+
+            actual = load_csr_chunk(
+                    row_spec=(subset[0][0], subset[0][1]),
+                    col_spec=(subset[1][0], subset[1][1]),
+                    data=written_zarr.X.data,
+                    indices=written_zarr.X.indices,
+                    indptr=written_zarr.X.indptr)
+
+            assert actual.sum() == expected_sum
+
+            np.testing.assert_array_equal(expected, actual)
