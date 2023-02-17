b2f8a9a6ced75a91fb338d3c473a3df5fb2357d3
danielsf <scott.daniel@alleninstitute.org>
2023-02-17 14:04:28 -0800

    the sorting of rows is a problem

--- cell_type_mapping_tree/tests/diff_exp/test_precompute.py
+++ cell_type_mapping_tree/tests/diff_exp/test_precompute.py
@@ -62,7 +62,7 @@ def test_precompute_smoketest(
         n_genes=ncols,
         output_path=output_path,
         n_processors=3,
-        rows_at_a_time=7)
+        rows_at_a_time=60)
 
     with h5py.File(output_path, 'r') as in_file:
         cluster_to_output = json.loads(
