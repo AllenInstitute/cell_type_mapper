4670c3ec5cb6de77fe1504712163e4ed0e570b41
danielsf <scott.daniel@alleninstitute.org>
2023-02-22 15:06:21 -0800

    one more test of separate_into_chunks

--- cell_type_mapping_tree/tests/utils/test_zarr_wrappers.py
+++ cell_type_mapping_tree/tests/utils/test_zarr_wrappers.py
@@ -21,8 +21,9 @@ def test_separate_into_chunks_qc():
 def test_separate_into_chunks():
 
     index_list = [5, 6, 7, 8, 9]
-    actual = separate_into_chunks(index_list=index_list,
-                                  chunk_shape=[3,])
+    actual = separate_into_chunks(
+                index_list=index_list,
+                chunk_shape=[3,])
 
     expected = [{'chunk_spec': ((3, 6),),
                  'output_loc': (0, 1),
@@ -33,5 +34,16 @@ def test_separate_into_chunks():
                 {'chunk_spec': ((9, 12),),
                  'output_loc': (4, 5),
                  'chunk_loc': (0, 1)}]
+
+    assert actual == expected
+
+    index_list = [12]
+    actual = separate_into_chunks(
+                index_list=index_list,
+                chunk_shape=[5,])
+
+    expected = [{'chunk_spec': ((10, 15),),
+                 'output_loc': (0, 1),
+                 'chunk_loc': (2, 3)}]
 
     assert actual == expected
