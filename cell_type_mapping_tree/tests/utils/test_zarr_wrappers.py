c7386b3a5603f438950fd851c7ddaf74c442a6a6
danielsf <scott.daniel@alleninstitute.org>
2023-02-22 15:26:23 -0800

    add test for ZarrCacheWrapper

--- cell_type_mapping_tree/tests/utils/test_zarr_wrappers.py
+++ cell_type_mapping_tree/tests/utils/test_zarr_wrappers.py
@@ -1,12 +1,57 @@
 import pytest
+import zarr
+import numpy as np
+import pathlib
+
+from hierarchical_mapping.utils.utils import (
+    _clean_up)
 
 from hierarchical_mapping.utils.zarr_wrappers import (
     ZarrCacheWrapper,
     separate_into_chunks)
 
 
-def test_zarr_cache_wrapper():
-    pass
+@pytest.fixture(scope='session')
+def zarr_path_fixture(tmp_path_factory):
+    tmp_dir = pathlib.Path(tmp_path_factory.mktemp('zarr_data'))
+    zarr_path = tmp_dir / 'eg.zarr'
+    rng = np.random.default_rng(3442123)
+    n = 237
+    with zarr.open(zarr_path ,'w') as out_file:
+        out_file.create(
+                name='data',
+                shape=(n),
+                chunks=30,
+                compressor=None,
+                dtype=np.float32)
+
+        out_file['data'][:] = rng.random(n)
+
+    yield zarr_path
+
+    _clean_up(tmp_dir)
+
+
+
+@pytest.mark.parametrize(
+        'i0, i1',
+        ((0, 15),
+         (25, 43),
+         (25, 72),
+         (180, 237),
+         (222, 237)))
+def test_zarr_cache_wrapper(
+        zarr_path_fixture,
+        i0,
+        i1):
+
+    wrapper = ZarrCacheWrapper(zarr_path_fixture / 'data')
+    actual = wrapper[i0:i1]
+
+    with zarr.open(zarr_path_fixture, 'r') as baseline_zarr:
+        expected = baseline_zarr['data'][i0:i1]
+
+    np.testing.assert_allclose(actual, expected)
 
 
 def test_separate_into_chunks_qc():
