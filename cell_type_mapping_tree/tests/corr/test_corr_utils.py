c5546e414acda9c42dbc6cafa243d0c3759e2b94
danielsf <scott.daniel@alleninstitute.org>
2023-03-06 13:31:34 -0800

    add utility to find genes that match between two datasets

---
+++ cell_type_mapping_tree/tests/corr/test_corr_utils.py
@@ -0,0 +1,26 @@
+import pytest
+
+import numpy as np
+
+from hierarchical_mapping.corr.utils import (
+    match_genes)
+
+
+@pytest.mark.parametrize(
+        "query, expected",
+        [(['b', 'x', 'f', 'e', 'w'],
+          {'reference': np.ndarray([1, 4, 5]),
+           'query': np.ndarray([0, 3, 2])})
+        ])
+def test_match_genes(
+        query,
+        expected):
+    reference_gene_names = ['a', 'b', 'c', 'd', 'e', 'f']
+    actual = match_genes(
+                reference_gene_names=reference_gene_names,
+                query_gene_names=query)
+
+    np.testing.assert_array_equal(actual['reference'],
+                                  expected['reference'])
+    np.testing.assert_array_equal(actual['query'],
+                                  expected['query'])
