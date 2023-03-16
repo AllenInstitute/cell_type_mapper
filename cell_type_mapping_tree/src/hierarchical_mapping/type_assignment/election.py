e692c5a2c3da7b3e20cee9537710d73bd4f2fa37
danielsf <scott.daniel@alleninstitute.org>
2023-03-15 20:23:02 -0700

    factor out method to tally votes

--- cell_type_mapping_tree/src/hierarchical_mapping/type_assignment/election.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/type_assignment/election.py
@@ -32,6 +32,46 @@ def choose_node(
     Array of cell type assignments (majority rule)
     """
 
+    votes = tally_votes(
+        query_gene_data=query_gene_data,
+        reference_gene_data=reference_gene_data,
+        bootstrap_factor=bootstrap_factor,
+        bootstrap_iteration=bootstrap_iteration,
+        rng=rng)
+
+    chosen_type = np.argmax(votes, axis=1)
+    result = [reference_types[ii] for ii in chosen_type]
+    return result
+
+
+def tally_votes(
+         query_gene_data,
+         reference_gene_data,
+         bootstrap_factor,
+         bootstrap_iteration,
+         rng):
+    """
+    Parameters
+    ----------
+    query_gene_data
+        cell-by-marker-gene array of query data
+    reference_gene_data
+        cell-by-marker-gene array of reference data
+    reference_types
+        array of cell types we are chosing from (n_cells in size)
+    bootstrap_factor
+        Factor by which to subsample reference genes at each bootstrap
+    bootstrap_iteration
+        Number of bootstrapping iterations
+    rng
+        random number generator
+
+    Returns
+    -------
+    Array of ints. Each row is a query cell. Each column is a
+    reference cell. The value is how many iterations voted for
+    "this query cell is the same type as this reference cell"
+    """
     n_markers = query_gene_data.shape[1]
     marker_idx = np.arange(n_markers)
     n_bootstrap = np.round(bootstrap_factor*n_markers).astype(int)
@@ -54,7 +94,4 @@ def choose_node(
             query_array=bootstrap_query)
 
         votes[query_idx, nearest_neighbors] += 1
-
-    chosen_type = np.argmax(votes, axis=1)
-    result = [reference_types[ii] for ii in chosen_type]
-    return result
+    return votes
