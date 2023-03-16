91f29f60bf9a7a9ae7a507850fe60bbf897a57f9
danielsf <scott.daniel@alleninstitute.org>
2023-03-15 21:06:23 -0700

    first test of assemble_query_data

--- cell_type_mapping_tree/src/hierarchical_mapping/type_assignment/marker_cache.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/type_assignment/marker_cache.py
@@ -15,8 +15,7 @@ from hierarchical_mapping.utils.multiprocessing_utils import (
     winnow_process_list)
 
 from hierarchical_mapping.utils.taxonomy_utils import (
-    get_all_leaf_pairs,
-    convert_tree_to_leaves)
+    get_all_leaf_pairs)
 
 from hierarchical_mapping.diff_exp.scores import (
     aggregate_stats,
@@ -45,54 +44,6 @@ def assign_types_to_chunk(
     mean_leaf_lookup = get_leaf_means(
         taxonomy_tree=taxonomy_tree,
         precompute_path=precompute_path)
-
-
-def assemble_query_data(
-        full_query_data,
-        mean_profile_lookup,
-        taxonomy_tree,
-        marker_cache_path,
-        parent_node):
-    """
-    Returns
-    --------
-    query_data
-        (n_query_cells, n_markers)
-    reference_data
-        (n_reference_cells, n_markers)
-    reference_types
-        At the level of this query (i.e. the direct
-        children of parent_node)
-    """
-
-    tree_as_leaves = convert_tree_to_leaves(taxonomy_tree)
-    hierarchy = taxonomy_tree['hierarchy']
-    level_to_idx = {level:idx for idx, level in enumerate(hierarchy)}
-
-    if parent_node is None:
-        parent_grp = 'None'
-        immediate_children = list(taxonomy_tree[hierarchy[0]].keys())
-        child_level = hierarchy[0]
-
-    else:
-        parent_grp = f"{parent_node[0]}/{parent_node[1]}"
-        immediate_children = list(taxonomy_tree[parent_node[0]].keys())
-        child_level = hierarchy[level_to_idx[parent_node[0]]+1]
-
-    immediate_children.sort()
-
-    leaf_to_type = dict()
-    for child in immediate_children:
-        for leaf in tree_as_leaves[child_level][child]:
-            leaf_to_type[leaf] = child
-
-    with h5py.File(marker_cache_path, 'r', swmr=True) as in_file:
-        reference_markers = in_file[parent_grp]['reference'][()]
-        query_markers = in_file[parent_grp]['query'][()]
-
-    query_data = full_query_data[:, query_markers]
-    # build reference data from mean_profile lookup
-    # build reference_types in parallel
 
 
 def create_marker_gene_cache(
