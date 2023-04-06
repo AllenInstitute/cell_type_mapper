37ff30c0320812543ecdb89c5ddac9b05421f9a2
danielsf <scott.daniel@alleninstitute.org>
2023-04-06 14:31:47 -0700

    copy over data files to fast scratch

--- cell_type_mapping_tree/garage/validation_test/run_assignment.py
+++ cell_type_mapping_tree/garage/validation_test/run_assignment.py
@@ -1,7 +1,10 @@
 import time
 import json
 import numpy as np
+import os
 import pathlib
+import tempfile
+import shutil
 
 from hierarchical_mapping.utils.taxonomy_utils import (
     convert_tree_to_leaves,
@@ -23,19 +26,32 @@ def main():
         '/allen/programs/celltypes/workgroups/rnaseqanalysis/changkyul/CIRRO/MFISH/atlas_brain_638850.remap.4334174.updated.imputed.h5ad')
     assert query_data_path.is_file()
 
+    tmp_dir = os.environ['TMPDIR']
+    query_tmp_path = tempfile.mkstemp(dir=tmp_dir, suffix='.h5ad')[1]
+    print(f"copying query_data to {query_tmp_path}")
+    shutil.copy(src=query_data_path, dst=query_tmp_path)
+    query_tmp_path = pathlib.Path(query_tmp_path)
+    assert query_tmp_path.is_file()
+    print('done copying')
 
     precomputed_path = data_dir / 'validation_test_precompute.h5'
     assert os.path.isfile(precomputed_path)
 
+    precomputed_tmp_path = tempfile.mkstemp(dir=tmp_dir, suffix='.h5')[1]
+    shutil.copy(src=precomputed_path, dst=precomputed_tmp_path)
 
-    t0 = time.time()
     marker_cache_path = data_dir / 'validation_marker_cache.h5'
     assert marker_cache_path.is_file()
+    marker_tmp_path = tempfile.mkstemp(dir=tmp_dir, suffix='.h5')[1]
+    shutil.copy(src=marker_cache_path, dst=marker_tmp_path)
 
+    print('copied all data')
+
+    t0 = time.time()
     result = run_type_assignment_on_h5ad(
-        query_h5ad_path=query_data_path,
-        precomputed_stats_path=precomputed_path,
-        marker_gene_cache_path=marker_cache_path,
+        query_h5ad_path=query_tmp_path,
+        precomputed_stats_path=precomputed_tmp_path,
+        marker_gene_cache_path=marker_tmp_path,
         taxonomy_tree=tree,
         n_processors=32,
         chunk_size=30000,
