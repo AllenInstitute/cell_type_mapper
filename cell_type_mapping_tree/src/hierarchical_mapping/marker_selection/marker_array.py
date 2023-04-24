f65e60d30ad2fd0c890511d3d4321908a543e5fb
danielsf <scott.daniel@alleninstitute.org>
2023-04-24 12:11:07 -0700

    draft implementation of new marker selection
    (untested)

--- cell_type_mapping_tree/src/hierarchical_mapping/marker_selection/marker_array.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/marker_selection/marker_array.py
@@ -2,7 +2,7 @@
 In this module, we provide a class MarkerArray that will serve as the
 accessor for the HDF5 file of marker genes produced by
 
-markers.find_markers_for_all_taxonomy_pairs
+diff_exp.markers.find_markers_for_all_taxonomy_pairs
 
 The point of this abstraction is that it will allow us to change
 the backend storage of marker gene data (should we decide another model
@@ -12,7 +12,6 @@ selects marker genes for a given query dataset.
 
 import h5py
 import json
-import numpy as np
 import pathlib
 
 from hierarchical_mapping.binary_array.backed_binary_array import (
@@ -28,7 +27,8 @@ class MarkerGeneArray(object):
     Parameters
     ----------
     cache_path:
-        path to the file created by markers.find_markers_for_all_taxonomy_pairs
+        path to the file created by
+        diff_exp.markers.find_markers_for_all_taxonomy_pairs
     """
     def __init__(self, cache_path):
         self.cache_path = pathlib.Path(cache_path)
@@ -37,7 +37,7 @@ class MarkerGeneArray(object):
                 f"{self.cache_path} is not a file")
 
         with h5py.File(self.cache_path, "r", swmr=True) as src:
-            self.gene_names = json.loads(
+            self._gene_names = json.loads(
                 src['gene_names'][()].decode('utf-8'))
             self.taxonomy_pair_to_idx = json.loads(
                 src['pair_to_idx'][()].decode('utf-8'))
@@ -46,23 +46,72 @@ class MarkerGeneArray(object):
         self.is_marker = BackedBinarizedBooleanArray(
             h5_path=self.cache_path,
             h5_group='markers/data',
-            n_rows=len(self.gene_names),
+            n_rows=self.n_genes,
             n_cols=self.n_pairs,
             read_only=True)
 
         self.up_regulated = BackedBinarizedBooleanArray(
             h5_path=self.cache_path,
             h5_group='up_regulated/data',
-            n_rows=len(self.gene_names),
+            n_rows=self.n_genes,
             n_cols=self.n_pairs,
             read_only=True)
 
-    def gene_idx_from_utility(
+    @property
+    def gene_names(self):
+        return self._gene_names
+
+    @property
+    def n_genes(self):
+        return len(self.gene_names)
+
+    def idx_of_pair(
             self,
             level,
             node1,
-            node2,
-            sign):
+            node2):
+        if node1 not in self.taxonomy_pair_to_idx[level]:
+            raise RuntimeError(
+                f"{node1} not under taxonomy level {level}")
+        if node2 not in self.taxnomy_pair_to_idx[level][node1]:
+            raise RuntimeError(
+                f"({level},  {node1}, {node2})\n"
+                "not a valid taxonomy pair specifcation; try reversing "
+                "node1 and node2")
+
+        pair_idx = self.taxonomy_pair_to_idx[level][node1][node2]
+        return pair_idx
+
+    def marker_mask_from_gene_idx(
+            self,
+            gene_idx):
+        marker_mask = self.is_marker.get_row(i_row=gene_idx)
+        up_mask = self.is_marker.get_row(i_row=gene_idx)
+        return marker_mask, up_mask
+
+    def marker_mask_from_pair_idx(
+            self,
+            pair_idx):
+        """
+        pair_idx is the index of the taxonomy pair.
+
+        sign is (+1, -1), indicating in which node the gene is
+        upsampled.
+
+        Returns (marker_mask, up_mask):
+        (n_genes,) array of booleans indicating
+                - is the gene a marker for this pair
+                - for which node in the pair is the gene up-regulated
+        """
+        marker_mask = self.is_marker.get_col(i_col=pair_idx)
+        up_mask = self.up_regulated.get_col(i_col=pair_idx)
+        return (marker_mask, up_mask)
+
+    def marker_mask_from_pair(
+            self,
+            level,
+            node1,
+            node2):
         """
         Get an array of gene indexes that match the utility
         specification.
@@ -80,27 +129,11 @@ class MarkerGeneArray(object):
 
         Returns
         -------
-        Numpy array of numerical indexes of genes that fit the utility
-        criterion
+        Returns (marker_mask, up_mask):
+        (n_genes,) array of booleans indicating
+                - is the gene a marker for this pair
+                - for which node in the pair is the gene up-regulated
         """
-        if sign not in (1, -1):
-            raise RuntimeError(
-                f"Do not know how to interpret sign={sign}\n"
-                "must be (+1 or -1)")
-
-        if node1 not in self.taxonomy_pair_to_idx[level]:
-            raise RuntimeError(
-                f"{node1} not under taxonomy level {level}")
-        if node2 not in self.taxnomy_pair_to_idx[level][node1]:
-            raise RuntimeError(
-                f"({level},  {node1}, {node2})\n"
-                "not a valid taxonomy pair specifcation; try reversing "
-                "node1 and node2")
-
-        pair_idx = self.taxonomy_pair_to_idx[level][node1][node2]
-        marker_mask = self.is_marker.get_col(i_col=pair_idx)
-        up_mask = self.up_regulated.get_col(i_col=pair_idx)
-        if sign < 0:
-            up_mask = np.logical_not(up_mask)
-        full_mask = np.logical_and(marker_mask, up_mask)
-        return np.where(full_mask)[0]
+        pair_idx = self.idx_of_pair(level=level, node1=node1, node2=node2)
+        return self.gene_idx_from_pair_idx(
+            pair_idx)
---
+++ cell_type_mapping_tree/src/hierarchical_mapping/marker_selection/selection.py
@@ -0,0 +1,241 @@
+import numpy as np
+
+from hierarchical_mapping.utils.taxonomy_utils import (
+    get_all_leaf_pairs)
+
+from hierarchical_mapping.corr.utils import (
+    match_genes)
+
+from hierarchical_mapping.marker_selection.utils import (
+    create_utility_array)
+
+
+def select_marker_genes_v2(
+        marker_gene_array,
+        query_gene_names,
+        taxonomy_tree,
+        parent_node,
+        n_per_utility=15):
+    """
+    Select marker genes given a reference set and a query set.
+
+    Each gene either is or is not a marker (no ranking by score).
+
+    Genes will be selected based on how many taxonomy pairs they
+    can distinguish between.
+
+    Try to find an equal number of up- and down- regulated markers
+    for each taxonomy pair (i.e. a balance of genes that are more
+    prominent in pair[0] versus more prominent in pair[1])
+
+    Parameters
+    ----------
+    marker_gene_array:
+        MarkerGeneArray providing access to data computed by
+
+        diff_exp.markers.find_markers_for_all_taxonomy_pairs.
+
+        This lists data about which genes are marker genes in
+        the reference dataset without regards to any query set.
+
+    query_gene_names:
+        List of gene names in a query set
+
+    taxonomy_tree:
+        dict encoding the taxonomy we are mapping to
+
+    parent_node:
+        (level, node) tuple indicating the parent for whose
+        children we are selecting markers (None will indicate
+        that we are at the root of the tree)
+
+    n_per_utility:
+        Number of marker genes to select per (node1, node2, +/-) set
+        (+/- indicates which node the gene is more prominent in)
+
+    Returns
+    -------
+    A list of marker gene names.
+        (Alphabetized for lack of a better ordering scheme.)
+    """
+
+    # get a numpy array of indices indicating which taxonomy
+    # pairs we need markers to discriminate between, given this
+    # parent node
+    taxonomy_idx_array = _get_taxonomy_idx(
+        taxonomy_tree=taxonomy_tree,
+        parent_node=parent_node,
+        marker_gene_array=marker_gene_array)
+
+    # mask across all available taxonomy pairs indicating
+    # which ones are actually being considered right now
+    taxonomy_mask = np.zeros(
+        marker_gene_array.n_pairs,
+        dtype=bool)
+    taxonomy_mask[taxonomy_idx_array] = True
+
+    # figure out which genes are in both the reference dataset
+    # and the query dataset
+    matched_genes = match_genes(
+        reference_gene_names=marker_gene_array.gene_names,
+        query_gene_names=query_gene_names)
+
+    reference_gene_mask = np.zeros(marker_gene_array.n_genes, dtype=bool)
+    reference_gene_mask[matched_genes['reference']] = True
+    if reference_gene_mask.sum() == 0:
+        raise RuntimeError(
+            "No gene overlap between reference and query set")
+
+    # calculate the initial array indicating how useful each gene
+    # (*all* reference genes at this point) is at discriminating
+    # between the taxonomy pairs that we cair about
+    utility_array = create_utility_array(
+            cache_path=marker_gene_array.cache_path,
+            gb_size=10,
+            taxonomy_mask=taxonomy_idx_array)
+
+    # mask out the genes which were not matched so that they
+    # are not selected
+    utility_array[np.logical_not[reference_gene_mask]] = 0
+
+    # tally how many markers are chosen for each taxonomy pair
+    # (the 2 columns are for up/down distinctions)
+    marker_counts = np.zeros((len(taxonomy_idx_array), 2), dtype=np.uint8)
+    been_filled = np.zeros((len(taxonomy_idx_array), 2), dtype=bool)
+    been_filled_size = been_filled.size
+
+    # the final result
+    marker_gene_idx_set = set()
+
+    # we will just start at the most useful gene and work our way down
+    sorted_utility_idx = list(np.argsort(utility_array))
+
+    while True:
+
+        # because the utility_array for genes that are not in the query
+        # set was set to zero at the beginning, this ought to indicate that
+        # none of the genes left have any utility in the taxonomy pairs
+        # we care about
+        if utility_array.max() <= 0:
+            break
+
+        # chose the gene with the largest utility
+        chosen_idx = sorted_utility_idx.pop(-1)
+        if chosen_idx in marker_gene_idx_set:
+            raise RuntimeError(
+                f"Something is wrong; chose gene {chosen_idx} twice")
+        marker_gene_idx_set.add(chosen_idx)
+
+        # so we do not choose this gene again
+        utility_array[chosen_idx] = 0
+
+        # update marker_counts
+        (marker_mask,
+         up_mask) = marker_gene_array.marker_mask_from_gene_idx(
+                         gene_idx=chosen_idx)
+
+        marker_mask = marker_mask[taxonomy_mask]
+        up_mask = up_mask[taxonomy_mask]
+
+        full_mask = np.logical_and(marker_mask, up_mask)
+        marker_counts[full_mask, 1] += 1
+        full_mask = np.logical_and(marker_mask, np.logical_not(up_mask))
+        marker_counts[full_mask, 0] += 1
+
+        # see if we have completed the desired complement of genes
+        # for any taxonomy pair
+        newly_full = np.where(
+            np.logical_and(
+                np.logical_not(been_filled),
+                marker_counts >= n_per_utility))
+
+        # if so, update the utility_array so that taxonomy pairs that
+        # already have their full complement of marker genes do not
+        # contribute to the utility score if genes
+        if len(newly_full[0]) > 0:
+            for pair_idx, raw_sign in zip(newly_full[0], newly_full[1]):
+                sign = {0: -1, 1: 1}[raw_sign]
+                utility_array = recalculate_utility_array(
+                    utility_array=utility_array,
+                    marker_gene_array=marker_gene_array,
+                    pair_idx=pair_idx,
+                    sign=sign)
+                been_filled[pair_idx, raw_sign] = True
+            sorted_utility_idx = list(np.argsort(utility_array))
+
+            if been_filled.sum() == been_filled_size:
+                # we have found all the genes we need
+                break
+
+    marker_gene_names = [
+        marker_gene_array.gene_names[idx]
+        for idx in marker_gene_idx_set]
+    marker_gene_names.sort()
+    return marker_gene_names
+
+
+def _get_taxonomy_idx(
+        taxonomy_tree,
+        parent_node,
+        marker_gene_array):
+    """
+    Return the numpy array of indexes for the leaf nodes
+    that need to be compared given the taxonomy tree
+    and the specified parent node
+    """
+    leaf_pairs = get_all_leaf_pairs(
+        taxonomy_tree=taxonomy_tree,
+        parent_node=parent_node)
+
+    taxonomy_idx_array = [
+        marker_gene_array.idx_of_pair(
+            leaf[0], leaf[1], leaf[2])
+        for leaf in leaf_pairs]
+
+    taxonomy_idx_array = np.array(
+        taxonomy_idx_array)
+
+    taxonomy_idx_array = np.sort(taxonomy_idx_array)
+    return taxonomy_idx_array
+
+
+def recalculate_utility_array(
+        utility_array,
+        marker_gene_array,
+        pair_idx,
+        sign):
+    """
+    utility_array is existing utility array
+
+    marker_gene_array is a MarkerGeneArray
+
+    pair_idx is the index of the taxonomy pair that has been fulfilled
+
+    sign is (+1, -1), indicating which node in the pair is up-regulated
+    in the gene
+
+
+    Returns
+    -------
+    utility array with the necessary rows (those that were markers
+    for the given (taxonomy_idx, sign) pair) decremented.
+
+    Notes
+    -----
+    This method also alters utility_array in place
+    """
+    if sign not in (1, -1):
+        raise RuntimeError(
+            f"Unclear how to interpret sign = {sign}\n"
+            "must be one of (-1, +1)")
+
+    (marker_mask,
+     up_mask) = marker_gene_array.marker_mask_from_pair_idx(
+                 pair_idx=pair_idx)
+
+    if sign > 0:
+        full_mask = np.logical_and(marker_mask, up_mask)
+    else:
+        full_mask = np.logical_and(marker_mask, np.logical_not(up_mask))
+    utility_array[full_mask] -= 1
+    return utility_array
