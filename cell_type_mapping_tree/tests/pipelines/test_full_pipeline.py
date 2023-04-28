6123b008ed3838be8b0c260177fe819d84c674f9
danielsf <scott.daniel@alleninstitute.org>
2023-04-28 13:03:40 -0700

    full test runs end-to-end

--- cell_type_mapping_tree/tests/pipelines/test_full_pipeline.py
+++ cell_type_mapping_tree/tests/pipelines/test_full_pipeline.py
@@ -2,11 +2,13 @@ import pytest
 
 import anndata
 import h5py
+import numpy as np
 import pathlib
 
 from hierarchical_mapping.utils.utils import (
     mkstemp_clean,
-    _clean_up)
+    _clean_up,
+    json_clean_dict)
 
 from hierarchical_mapping.utils.taxonomy_utils import (
     get_taxonomy_tree)
@@ -19,6 +21,9 @@ from hierarchical_mapping.diff_exp.markers import (
 
 from hierarchical_mapping.type_assignment.marker_cache_v2 import (
     create_marker_gene_cache_v2)
+
+from hierarchical_mapping.type_assignment.election import (
+    run_type_assignment_on_h5ad)
 
 
 @pytest.fixture
@@ -34,9 +39,11 @@ def tmp_dir_fixture(
 def taxonomy_tree_fixture(
         records_fixture,
         column_hierarchy):
-    return get_taxonomy_tree(
+    tree = get_taxonomy_tree(
         obs_records=records_fixture,
         column_hierarchy=column_hierarchy)
+    tree = json_clean_dict(tree)
+    return tree
 
 
 def test_all_of_it(
@@ -79,8 +86,8 @@ def test_all_of_it(
         assert in_file['up_regulated/data'][()].sum() > 0
         assert in_file['markers/data'][()].sum() > 0
 
-    a_data = anndata.read_h5ad(query_h5ad_path_fixture, backed='r')
-    query_gene_names = list(a_data.var_names)
+    q_data = anndata.read_h5ad(query_h5ad_path_fixture, backed='r')
+    query_gene_names = list(q_data.var_names)
 
     marker_cache_path = mkstemp_clean(
         dir=tmp_dir_fixture,
@@ -98,3 +105,23 @@ def test_all_of_it(
 
     with h5py.File(marker_cache_path, 'r') as in_file:
         assert len(in_file['None']['reference'][()]) > 0
+
+    result = run_type_assignment_on_h5ad(
+        query_h5ad_path=query_h5ad_path_fixture,
+        precomputed_stats_path=precomputed_path,
+        marker_gene_cache_path=marker_cache_path,
+        taxonomy_tree=taxonomy_tree_fixture,
+        n_processors=3,
+        chunk_size=100,
+        bootstrap_factor=0.9,
+        bootstrap_iteration=100,
+        rng=np.random.default_rng(123545))
+
+    assert len(result) == q_data.X.shape[0]
+    cell_id_set = set()
+    for cell in result:
+        cell_id_set.add(cell['cell_id'])
+        for level in column_hierarchy:
+            assert level in cell
+            assert cell[level]['assignment'] is not None
+    assert cell_id_set == set(q_data.obs_names)
