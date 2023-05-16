b5e44876c0354a52fc2002133c171ee491449d3c
danielsf <scott.daniel@alleninstitute.org>
2023-05-16 08:57:37 -0700

    document AnnDataRowIterator

--- cell_type_mapping_tree/src/hierarchical_mapping/anndata_iterator/anndata_iterator.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/anndata_iterator/anndata_iterator.py
@@ -22,6 +22,18 @@ class AnnDataRowIterator(object):
     file. If the anndata file is CSC, it will, as a first step,
     write the data out to a tempfile in CSR (or dense) format
     for more rapid iteration over rows.
+
+    Parameters
+    ----------
+    h5ad_path:
+        Path to the h5ad file over whose rows we are iterating
+    row_chunk_size:
+        Number of rows to deliver per chunk
+    tmp_dir:
+        Optional scratch directory. This is where a hypothetical
+        CSC file will be written as a CSR file. If None, the
+        CSC file will be iterated over using anndata's infrastructure
+        (which can be very slow)
     """
 
     def __init__(
@@ -64,6 +76,14 @@ class AnnDataRowIterator(object):
         return self
 
     def __next__(self):
+        """
+        Return the next chunk of rows.
+
+        Actually return a tuple
+        (row_chunk, r0, r1)
+        where r0 and r1 are the indices of the slice of rows
+        (i.e. row_chunk is data[r0:r1, :])
+        """
         result = next(self._chunk_iterator)
         if self._iterator_type == 'anndata':
             r0 = result[1]
@@ -74,7 +94,21 @@ class AnnDataRowIterator(object):
             result = (result, r0, r1)
         return result
 
-    def _initialize_anndata_iterator(self, h5ad_path, row_chunk_size):
+    def _initialize_anndata_iterator(
+            self,
+            h5ad_path,
+            row_chunk_size):
+        """
+        Initialize iterator using anndata.chunked_X
+        directly.
+
+        Parameters
+        ----------
+        h5ad_path:
+            Path to h5ad file whose rows we are iterating over
+        row_chunk_size:
+            Number of rows to return per chunk
+        """
         self._iterator_type = 'anndata'
         data = anndata.read_h5ad(h5ad_path, backed='r')
         self.n_rows = data.X.shape[0]
@@ -86,26 +120,43 @@ class AnnDataRowIterator(object):
             h5ad_path,
             row_chunk_size,
             tmp_dir=None):
-        self._iterator_type = 'anndata'
+        """
+        Initialize iterator for CSC data. If possible,
+        write out data to scratch space as CSR matrix
+        and initialize iterator over that.
+
+        Parameters
+        ----------
+        h5ad_path:
+            Path to h5ad file whose rows we are iterating over
+        row_chunk_size:
+            Number of rows to deliver per chunk
+        tmp_dir:
+            scratch dir in which to write the CSR form of the data
+            (if None, no CSR data will be written and we will just
+            use anndata.chunked_X to iterate over the data)
+        """
+        write_as_csr = True
         if tmp_dir is None:
-            self._initialize_anndata_iterator(
-                h5ad_path=h5ad_path,
-                row_chunk_size=row_chunk_size)
+            write_as_csr = False
+
         else:
             with h5py.File(h5ad_path, 'r') as src:
                 attrs = dict(src['X'].attrs)
 
             if 'shape' not in attrs:
-                self._initialize_anndata_iterator(
-                    h5ad_path=h5ad_path,
-                    row_chunk_size=row_chunk_size)
-                return
+                write_as_csr = False
 
+        if not write_as_csr:
+            self._initialize_anndata_iterator(
+                h5ad_path=h5ad_path,
+                row_chunk_size=row_chunk_size)
+        else:
             self.tmp_dir = tempfile.mkdtemp(dir=tmp_dir)
             self.tmp_path = pathlib.Path(
                 mkstemp_clean(
                     dir=self.tmp_dir,
-                    prefix=f"{h5ad_path.name}_as_dense_",
+                    prefix=f"{h5ad_path.name}_as_csr_",
                     suffix=".h5"))
 
             t0 = time.time()
@@ -132,8 +183,27 @@ class AnnDataRowIterator(object):
 
 
 class CSRRowIterator(object):
+    """
+    Class to iterate over a CSR matrix using h5py to directly
+    access the data (rather than anndata, which can load unnecessary
+    data into memory)
 
-    def __init__(self, h5_path, row_chunk_size, array_shape):
+    Parameters
+    ----------
+    h5_path:
+        Path to HDF5 file containing CSR matrix data
+    row_chunk_size:
+        Number of rows to return with each chunk
+    array_shape:
+        Shape of the array we are iterating over
+    """
+
+    def __init__(
+            self,
+            h5_path,
+            row_chunk_size,
+            array_shape):
+
         self.h5_path = h5_path
         self.h5_handle = None
         self.row_chunk_size = row_chunk_size
@@ -148,6 +218,14 @@ class CSRRowIterator(object):
             self.h5_handle = None
 
     def __next__(self):
+        """
+        Actually return a tuple
+
+        (row_chunk, r0, r1)
+
+        where r0 and r1 are the indices of the slice of rows
+        (i.e. row_chunk is data[r0:r1, :])
+        """
         if self.r0 >= self.n_rows:
             if self.h5_handle is not None:
                 self.h5_handle.close()
