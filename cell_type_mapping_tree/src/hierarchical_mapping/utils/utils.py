f06b1825eafa22cdf06ca8f9dde8d211801ab2b9
danielsf <scott.daniel@alleninstitute.org>
2023-06-09 11:23:55 -0700

    make sure month, day, hour, etc. have 2 digits in timestamp

--- cell_type_mapping_tree/src/hierarchical_mapping/utils/utils.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/utils/utils.py
@@ -157,6 +157,7 @@ def get_timestamp():
     Return a string with the current timestamp
     """
     now = datetime.datetime.now()
-    result = f"{now.year}-{now.month}-{now.day}-{now.hour}"
-    result += f"-{now.minute}-{now.second}"
+    result = f"{now.year:04d}-{now.month:02d}"
+    result += f"-{now.day:02d}-{now.hour:02d}"
+    result += f"-{now.minute:02d}-{now.second:02d}"
     return result
