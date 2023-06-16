c94514d62ee899cc9012dd8ab23f57e6ee0be7d1
danielsf <scott.daniel@alleninstitute.org>
2023-06-16 11:23:20 -0700

    keep exploring the marker genes we have

--- cell_type_mapping_tree/garage/taxonomy_data_release/test_markers.py
+++ cell_type_mapping_tree/garage/taxonomy_data_release/test_markers.py
@@ -27,26 +27,70 @@ def main():
     cell_metadata = data_dir / "WMB-10X/20230630/cell_metadata.csv"
     assert cell_metadata.is_file()
 
-    hierarchy=[
-        "CCN20230504_CLAS",
-        "CCN20230504_SUBC",
-        "CCN20230504_CLUS"]
-
-    level_to_idx = {n:ii+1 for ii, n in enumerate(hierarchy)}
-
-    alias_mapper = get_alias_mapper(
-        csv_path=cluster_membership,
-        valid_term_set_labels=hierarchy,
-        alias_column_name='cluster_annotation_term_name',
-        strict_alias=True)
-
     taxonomy_tree = TaxonomyTree.from_data_release(
         cell_metadata_path=cell_metadata,
         cluster_annotation_path=cluster_annotation,
         cluster_membership_path=cluster_membership,
-        hierarchy=hierarchy)
+        hierarchy=[
+        "CCN20230504_CLAS",
+        "CCN20230504_SUBC",
+        "CCN20230504_SUPT",
+        "CCN20230504_CLUS"])
 
-    #parent_list =
+    taxonomy_tree = taxonomy_tree.drop_level("CCN20230504_SUPT")
+    print(taxonomy_tree.hierarchy)
+
+    alias_mapper = get_alias_mapper(
+        csv_path=cluster_membership,
+        valid_term_set_labels=taxonomy_tree.hierarchy,
+        alias_column_name='cluster_annotation_term_name',
+        strict_alias=True)
+
+    level_to_idx = {n:ii+2 for ii, n in enumerate(taxonomy_tree.hierarchy)}
+    print(level_to_idx)
+    good_ct = 0
+    bad_ct = 0
+    parent_list = taxonomy_tree.all_parents
+
+    expected_file_names = []
+    missing_file_names = []
+    for parent_node in parent_list:
+        if parent_node is None:
+            fname = 'marker.1.root.csv'
+        else:
+            children = taxonomy_tree.children(parent_node[0], parent_node[1])
+            if len(children) < 2:
+                continue
+            level_idx = level_to_idx[parent_node[0]]
+            readable_name = alias_mapper[(parent_node[0], parent_node[1])]
+            prefix = readable_name.split()[0]
+            readable_name = readable_name.replace(f'{prefix} ','')
+            munged = readable_name.replace(' ','+').replace('/','__')
+            fname = f'marker.{level_idx}.{munged}.csv'
+        fpath = marker_dir / fname
+        expected_file_names.append(fname)
+        if fpath.is_file():
+            good_ct += 1
+        else:
+            bad_ct += 1
+            missing_file_names.append(fname)
+            print(f"{fname} does not exist")
+    print(f"good {good_ct} bad {bad_ct}")
+    missing_file_names.sort()
+    expected_file_names.sort()
+    str_dir = str(marker_dir.resolve().absolute())
+    with open("expected_marker_files.csv", "w") as dst:
+        dst.write("# list of expected file names\n")
+        dst.write(f"# in dir {str_dir}\n")
+        for f in expected_file_names:
+            dst.write(f"{f}\n")
+
+    with open("missing_marker_files.csv", "w") as dst:
+        dst.write("# list of missing file names\n")
+        dst.write(f"# in dir {str_dir}\n")
+        for f in missing_file_names:
+            dst.write(f"{f}\n")
+ 
 
     print("all done")
 
