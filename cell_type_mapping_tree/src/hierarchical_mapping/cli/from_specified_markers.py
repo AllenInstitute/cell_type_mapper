020a97b687fc25ee0f0c031c60e68ce14c62f186
danielsf <scott.daniel@alleninstitute.org>
2023-06-22 08:44:38 -0700

    move blob_to_csv back into try: block (rather than finally:)
    the reason is that, if an error occurs, it will likely have mucked
    up the output such that blob_to_csv cannot run

--- cell_type_mapping_tree/src/hierarchical_mapping/cli/from_specified_markers.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/cli/from_specified_markers.py
@@ -197,6 +197,14 @@ def run_mapping(config, output_path, log_path=None):
         output["marker_genes"] = type_assignment["marker_genes"]
         csv_result["taxonomy_tree"] = type_assignment["taxonomy_tree"]
         csv_result["assignments"] = assignments
+
+        if config['csv_result_path'] is not None:
+            blob_to_csv(
+                results_blob=csv_result.get("assignments"),
+                taxonomy_tree=csv_result.get("taxonomy_tree"),
+                output_path=config['csv_result_path'],
+                metadata_path=config['extended_result_path'])
+
         log.info("RAN SUCCESSFULLY")
     except Exception:
         traceback_msg = "an ERROR occurred ===="
@@ -212,13 +220,6 @@ def run_mapping(config, output_path, log_path=None):
         output["log"] = log.log
         with open(output_path, "w") as out_file:
             out_file.write(json.dumps(output, indent=2))
-
-        if config['csv_result_path'] is not None:
-            blob_to_csv(
-                results_blob=csv_result.get("assignments"),
-                taxonomy_tree=csv_result.get("taxonomy_tree"),
-                output_path=config['csv_result_path'],
-                metadata_path=config['extended_result_path'])
 
 
 def _run_mapping(config, tmp_dir, log):
