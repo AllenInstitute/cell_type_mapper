8087e587deb651bb853fe955e720b08dee3dd215
danielsf <scott.daniel@alleninstitute.org>
2023-06-26 11:50:30 -0700

    ignore 'metadata' in marker lookup specification

--- cell_type_mapping_tree/src/hierarchical_mapping/cli/from_specified_markers.py
+++ cell_type_mapping_tree/src/hierarchical_mapping/cli/from_specified_markers.py
@@ -313,6 +313,8 @@ def _run_mapping(config, tmp_dir, log):
 
         all_markers = set()
         for k in marker_lookup:
+            if k == 'metadata':
+                continue
             all_markers = all_markers.union(set(marker_lookup[k]))
         all_markers = list(all_markers)
         all_markers.sort()
