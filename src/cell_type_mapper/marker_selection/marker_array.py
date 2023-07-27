55f423df876ea54b189cf10309f7a68654029f74
danielsf <scott.daniel@alleninstitute.org>
2023-07-27 15:28:54 -0700

    move marker array utility functions into their own module

--- src/cell_type_mapper/marker_selection/marker_array.py
+++ src/cell_type_mapper/marker_selection/marker_array.py
@@ -22,6 +22,10 @@ from cell_type_mapper.binary_array.binary_array import (
 from cell_type_mapper.diff_exp.sparse_markers_by_pair import (
     SparseMarkersByPair)
 
+from cell_type_mapper.marker_selection.marker_array_utils import (
+    _create_new_pair_lookup,
+    _idx_of_pair)
+
 
 class MarkerGeneArray(object):
     """
@@ -366,39 +370,3 @@ class MarkerGeneArray(object):
         np.ndarray
         """
         return self.is_marker.get_row_batch(gene0, gene1)
-
-
-def _create_new_pair_lookup(only_keep_pairs):
-    """
-    Create new pair-to-idx lookup for case where we
-    are only keeping the specified pairs
-    """
-    new_lookup = dict()
-    for ii, pair in enumerate(only_keep_pairs):
-        level = pair[0]
-        node1 = pair[1]
-        node2 = pair[2]
-        if level not in new_lookup:
-            new_lookup[level] = dict()
-        if node1 not in new_lookup[level]:
-            new_lookup[level][node1] = dict()
-        new_lookup[level][node1][node2] = ii
-    return new_lookup
-
-
-def _idx_of_pair(
-        taxonomy_pair_to_idx,
-        level,
-        node1,
-        node2):
-    if node1 not in taxonomy_pair_to_idx[level]:
-        raise RuntimeError(
-            f"{node1} not under taxonomy level {level}")
-    if node2 not in taxonomy_pair_to_idx[level][node1]:
-        raise RuntimeError(
-            f"({level},  {node1}, {node2})\n"
-            "not a valid taxonomy pair specification; try reversing "
-            "node1 and node2")
-
-    pair_idx = taxonomy_pair_to_idx[level][node1][node2]
-    return pair_idx
