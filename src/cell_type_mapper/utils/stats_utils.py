fix bug in boring_t calculation
(was returning negative value; expect positive value)

src/cell_type_mapper/utils/stats_utils.py
@@ -311,4 +311,4 @@ def boring_t_from_p_value(p_value):
     fine_t = np.linspace(gross_t[gross_idx-1], gross_t[gross_idx+1], 1000)
     fine_cdf = scipy_stats.norm.cdf(fine_t)
     value = np.interp(0.5*p_value, fine_cdf, fine_t)
-    return value
+    return -1*value

tests/utils/test_t_test_code.py
@@ -122,6 +122,7 @@ def test_when_boring_t_is_zero():
     'p_value', [0.01, 0.02, 0.005, 0.003])
 def test_boring_t_calc(p_value):
     boring_t = stats_utils.boring_t_from_p_value(p_value)
-    truth = scipy.stats.norm.cdf(boring_t)
+    assert boring_t > 0
+    truth = scipy.stats.norm.cdf(-1*boring_t)
     np.testing.assert_allclose(
         truth, 0.5*p_value, atol=0.0, rtol=1.0e-3)