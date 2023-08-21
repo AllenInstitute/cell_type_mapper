545083bee6f708b1d996a3c3a6584f7a13a5fb64
danielsf <scott.daniel@alleninstitute.org>
2023-08-21 13:20:27 -0700

    redirect data product generation scriptswq

--- garage/abc_230830/marker_cache.sh
+++ garage/abc_230830/marker_cache.sh
@@ -1,8 +1,8 @@
 python -m cell_type_mapper.cli.marker_cache_from_csv_dir \
---marker_dir /allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/Taxonomies/AIT21.0_root/nlevel4_marker_index/marker_list_on_nodes \
---precomputed_file_path /allen/aibs/technology/danielsf/knowledge_base/scratch/abc_revision_230830/abc_stats_230807.h5 \
+--marker_dir /allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/Taxonomies/AIT21.0_root_curated/nlevel4_marker_index/marker_list_on_nodes \
+--precomputed_file_path /allen/aibs/technology/danielsf/knowledge_base/handoff_230821/precomputed_stats_ABC_revision_230821.h5 \
 --drop_level CCN20230722_SUPT \
---output_path /allen/aibs/technology/danielsf/knowledge_base/scratch/abc_revision_230830/mouse_markers_230809_old_subclass.json
+--output_path /allen/aibs/technology/danielsf/knowledge_base/handoff_230821/mouse_markers_230821.json
 
 
 #--marker_dir /allen/programs/celltypes/workgroups/rnaseqanalysis/shiny/Taxonomies/AIT21.0.root_mouse/curated_root_21.0_nlevel4_marker_index/marker_list_on_nodes/ \
