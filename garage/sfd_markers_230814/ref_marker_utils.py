a085502b4e201d107974c5d18826a3e336ae16ef
danielsf <scott.daniel@alleninstitute.org>
2023-08-25 09:13:12 -0700

    diagnostic utility can compare de p values

--- garage/sfd_markers_230814/ref_marker_utils.py
+++ garage/sfd_markers_230814/ref_marker_utils.py
@@ -18,6 +18,9 @@ from cell_type_mapper.diff_exp.scores import (
     read_precomputed_stats,
     diffexp_p_values)
 
+from cell_type_mapper.de_bayes.de_ebayes import (
+    de_pairs_ebayes)
+
 def marker_stats(
         cl0,
         cl1,
@@ -73,7 +76,32 @@ def marker_stats(
         var2=stats2['var'],
         n2=stats2['n_cells'])
 
+    gene_names = [f'g_{ii}' for ii in range(len(stats['mean1']))]
+    pairs = [(cl0, cl1)]
+
+    cl_mean_data = []
+    cl_var_data = []
+    for cl, stats in zip((cl0, cl1), (stats1, stats2)):
+        this_mean = {'cluster_name': cl}
+        this_var = {'cluster_name': cl}
+        for ii, gene in enumerate(gene_names):
+            this_mean[gene] = stats['mean'][ii]
+            this_var[gene] = stats['var'][ii]
+        cl_mean_data.append(this_mean)
+        cl_var_data.append(this_var)
+    cl_means = pd.DataFrame(cl_mean_data).set_index('cluster_name')
+    cl_vars = pd.DataFrame(cl_var_data).set_index('cluster_name')
+    cl_size = {cl0: stats1['n_cells'], cl1: stats2['n_cells']}
+
+    de_p_vals = de_pairs_ebayes(
+        pairs=pairs,
+        cl_means=cl_means,
+        cl_vars=cl_vars,
+        cl_size=cl_size,
+        p_th=p_th)
+
     return {'p_values': p_values,
+            'de_p_values': de_p_vals[pairs[0]],
             'q1': q1,
             'qdiff': qdiff,
             'n0': stats1['n_cells'],
