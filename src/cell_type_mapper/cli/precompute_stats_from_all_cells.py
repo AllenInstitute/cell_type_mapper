d4e72ff601ab0d2ca782e60f763daa9efd0ba217
danielsf <scott.daniel@alleninstitute.org>
2023-09-20 15:11:46 -0700

    precompute CLI can split by dataset_label

--- src/cell_type_mapper/cli/precompute_stats_from_all_cells.py
+++ src/cell_type_mapper/cli/precompute_stats_from_all_cells.py
@@ -3,6 +3,7 @@ import copy
 import h5py
 import json
 from marshmallow import post_load
+import pandas as pd
 import pathlib
 
 from cell_type_mapper.utils.utils import (
@@ -80,6 +81,17 @@ class PrecomputedStatsSchema(argschema.ArgSchema):
             "Set to True to allow the code to overwrite an existing file."
         ))
 
+    split_by_dataset = argschema.fields.Boolean(
+        required=False,
+        default=False,
+        allow_none=False,
+        description=(
+            "If true, split the dataset by the 'dataset_label' field in "
+            "cell_metadata.csv, storing each dataset in a separate HDF5 file. "
+            "Files will be named like ouptut_path but with a secondary suffix "
+            "added before .h5 specifying which dataset they contain."
+        ))
+
     @post_load
     def check_norm(self, data, **kwargs):
         if data['normalization'] not in ('raw', 'log2CPM'):
@@ -90,17 +102,42 @@ class PrecomputedStatsSchema(argschema.ArgSchema):
 
     @post_load
     def check_output(self, data, **kwargs):
-        output_path = pathlib.Path(data['output_path'])
-        if output_path.exists():
-            if not data['clobber']:
-                raise RuntimeError(
-                    f"{output_path} already exists; run with clobber=True "
-                    "to overwite")
+        final_output_lookup = dict()
+        run_default = False
+        if data['split_by_dataset']:
+            df = pd.read_csv(data['cell_metadata_path'])
+            if 'dataset_label' in df.columns:
+                dataset_values = set(df.dataset_label.values)
+                baseline_path = pathlib.Path(data['output_path'])
+                output_parent = baseline_path.absolute().resolve().parent
+                baseline_suffix = baseline_path.suffix
+                baseline_stem = baseline_path.stem
+                for dataset in dataset_values:
+                    new_suffix = f'{dataset}{baseline_suffix}'
+                    new_name = f'{baseline_stem}.{new_suffix}'
+                    final_output_lookup[dataset] = str(output_parent/new_name)
+            else:
+                run_default = True
+        else:
+            run_default = True
+
+        if run_default:
+            output_path = pathlib.Path(data['output_path'])
+            final_output_lookup['None'] = str(output_path.resolve().absolute())
 
         # make sure we can write here
-        with open(output_path, 'wb') as dst:
-            dst.write(b'gar')
+        for output_path in final_output_lookup:
+            output_path = pathlib.Path(output_path)
+            if output_path.exists():
+                if not data['clobber']:
+                    raise RuntimeError(
+                        f"{output_path} already exists; run with clobber=True "
+                        "to overwite")
 
+            with open(output_path, 'wb') as dst:
+                dst.write(b'gar')
+
+        data['final_output_path'] = final_output_lookup
         return data
 
 
@@ -119,19 +156,36 @@ class PrecomputationRunner(argschema.ArgSchemaParser):
            cluster_membership_path=self.args['cluster_membership_path'],
            hierarchy=self.args['hierarchy'])
 
-        precompute_summary_stats_from_h5ad_list_and_tree(
-            data_path_list=self.args['h5ad_path_list'],
-            taxonomy_tree=taxonomy_tree,
-            rows_at_a_time=10000,
-            normalization=self.args['normalization'],
-            output_path=self.args['output_path'])
+        cell_metadata = pd.read_csv(self.args['cell_metadata_path'])
+        dataset_to_cell_set = dict()
+        if 'dataset_label' in cell_metadata.columns:
+            for cell_id, dataset_label in zip(
+                            cell_metadata.cell_label.values,
+                            cell_metadata.dataset_label.values):
+                if dataset_label not in dataset_to_cell_set:
+                    dataset_to_cell_set[dataset_label] = set()
+                dataset_to_cell_set[dataset_label].add(cell_id)
 
-        metadata['timestamp'] = get_timestamp()
+        for dataset in self.args['final_output_path'].keys():
+            output_path = self.args['final_output_path'][dataset]
+            if dataset in dataset_to_cell_set:
+                cell_set = dataset_to_cell_set[dataset]
+            else:
+                cell_set = None
+            precompute_summary_stats_from_h5ad_list_and_tree(
+                data_path_list=self.args['h5ad_path_list'],
+                taxonomy_tree=taxonomy_tree,
+                rows_at_a_time=10000,
+                normalization=self.args['normalization'],
+                output_path=output_path,
+                cell_set=cell_set)
 
-        with h5py.File(self.args['output_path'], 'a') as out_file:
-            out_file.create_dataset(
-                'metadata',
-                data=json.dumps(metadata).encode('utf-8'))
+            metadata['timestamp'] = get_timestamp()
+
+            with h5py.File(output_path, 'a') as out_file:
+                out_file.create_dataset(
+                    'metadata',
+                    data=json.dumps(metadata).encode('utf-8'))
 
 
 def main():
