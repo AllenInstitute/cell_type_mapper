2b56119c9f176882f4a31f36a44fdfd15c379b77
danielsf <scott.daniel@alleninstitute.org>
2023-10-02 10:59:34 -0700

    raise an error if query data has min value < 0

--- src/cell_type_mapper/type_assignment/election_runner.py
+++ src/cell_type_mapper/type_assignment/election_runner.py
@@ -6,6 +6,9 @@ from cell_type_mapper.utils.torch_utils import (
     is_torch_available,
     use_torch)
 
+from cell_type_mapper.validation.utils import (
+    get_minmax_x_from_h5ad)
+
 if is_torch_available():
     from cell_type_mapper.gpu_utils.type_assignment.election import (
         run_type_assignment_on_h5ad_gpu)
@@ -27,6 +30,31 @@ def run_type_assignment_on_h5ad(
         log=None,
         max_gb=10,
         results_output_path=None):
+
+    if normalization not in ('raw', 'log2CPM'):
+        error_msg = (
+            f"Do not know how to handle normalization = '{normalization}'; "
+            "must be either 'raw' or 'log2CPM'"
+        )
+        if log is not None:
+            log.error(error_msg)
+        else:
+            raise RuntimeError(error_msg)
+
+    if normalization == 'raw':
+        # check that data is >= 0
+        minmax = get_minmax_x_from_h5ad(query_h5ad_path)
+        if minmax[0] < 0:
+            error_msg = (
+                f"Minimum expression value is {minmax[0]}; "
+                "must be >= 0 (we will be taking the logarithm "
+                "in order to convert from 'raw' to 'log2CPM' data)"
+            )
+            if log is not None:
+                log.error(error_msg)
+            else:
+                raise RuntimeError(error_msg)
+
     if use_torch():
         return run_type_assignment_on_h5ad_gpu(
             query_h5ad_path,
