a74c82c7e7de5d9edbc699b2be8d5d670081e661
danielsf <scott.daniel@alleninstitute.org>
2023-10-09 19:26:23 -0700

    fix new query marker validation scheme (and unit tests)

--- tests/type_assignment/test_marker_cache_v2.py
+++ tests/type_assignment/test_marker_cache_v2.py
@@ -92,7 +92,7 @@ def test_validate_marker_lookup(
                 query_gene_names=query_gene_fixture)
 
     # test case where a parent has no valid markers in query gene set
-    for to_drop in ('None', 'class/A', 'subclass/d'):
+    for to_drop in ('None', ):
         lookup = copy.deepcopy(complete_lookup_fixture)
         lookup.pop(to_drop)
         lookup[to_drop] = [f'j_{ii}' for ii in range(3)]
@@ -101,6 +101,36 @@ def test_validate_marker_lookup(
                 marker_lookup=lookup,
                 taxonomy_tree=taxonomy_tree_fixture,
                 query_gene_names=query_gene_fixture)
+
+    # test patching of missing markers
+    for to_drop in ('class/A', 'subclass/f'):
+        lookup = copy.deepcopy(complete_lookup_fixture)
+        lookup.pop(to_drop)
+        lookup[to_drop] = [f'j_{ii}' for ii in range(3)]
+        validate_marker_lookup(
+                marker_lookup=lookup,
+                taxonomy_tree=taxonomy_tree_fixture,
+                query_gene_names=query_gene_fixture)
+
+        if to_drop == 'class/A':
+            assert lookup['class/A'] == lookup['None']
+        else:
+            assert lookup['subclass/f'] == lookup['class/C']
+
+    # test case where we have to skip two levels to get markers
+    lookup = copy.deepcopy(complete_lookup_fixture)
+    nonsense = [f'j_{ii}' for ii in range(4)]
+    lookup['class/C'] = nonsense
+    lookup['subclass/f'] = nonsense
+
+    validate_marker_lookup(
+                marker_lookup=lookup,
+                taxonomy_tree=taxonomy_tree_fixture,
+                query_gene_names=query_gene_fixture)
+
+    assert lookup['class/C'] == lookup['None']
+    assert lookup['subclass/f'] == lookup['None']
+    assert lookup['subclass/e'] != lookup['None']
 
     # test case where a parent has entry in lookup, but is blank
     for to_drop in ('None', 'class/A', 'subclass/d'):
