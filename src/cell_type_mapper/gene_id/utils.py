f7bfee237576703ded0e498f0b8c47e4f8b43849
danielsf <scott.daniel@alleninstitute.org>
2023-10-20 14:33:08 -0700

    add function to detect species from list of gene identifiers

--- src/cell_type_mapper/gene_id/utils.py
+++ src/cell_type_mapper/gene_id/utils.py
@@ -1,5 +1,11 @@
 import re
 
+from cell_type_mapper.data.mouse_gene_id_lookup import (
+    mouse_gene_id_lookup)
+
+from cell_type_mapper.data.human_gene_id_lookup import (
+    human_gene_id_lookup)
+
 
 def is_ensembl(gene_id):
     """
@@ -7,8 +13,70 @@ def is_ensembl(gene_id):
     gene_id is an Ensembl identifier
     """
     if not hasattr(is_ensembl, 'pattern'):
-        is_ensembl.pattern = re.compile('ENS[A-Z]+[0-9]+(\.[0-9]+)?')   # noqa W605
+        is_ensembl.pattern = re.compile(r'ENS[A-Z]+[0-9]+(\.[0-9]+)?')   # noqa W605
     match = is_ensembl.pattern.fullmatch(gene_id)
     if match is None:
         return False
     return True
+
+
+def detect_species(gene_id_list):
+    """
+    Take a list of arbitrary gene identifiers (strings). Return a string
+    indicating if the genes are from the 'mouse' or 'human' genome.
+
+    Decision is as follows:
+
+    - If any known mouse EnsemblIDs are present, return 'mouse'
+    - If any known human EnsemblIDs are present, return 'human'
+    - If EnsemblIDs from human and mouse are present, raise error
+    - If there are more mouse than human gene symbols, return 'mouse'
+    - If there are more human than mouse gene symbols, return 'human'
+    - If there are no known gene symbols present, return None
+    """
+
+    gene_set = set(gene_id_list)
+    census = dict()
+    for species, lookup in [('mouse', mouse_gene_id_lookup),
+                            ('human', human_gene_id_lookup)]:
+
+        ens = set(lookup.values())
+        symb = set(lookup.keys())
+        n_ens = len(ens.intersection(gene_set))
+        n_symb = len(symb.intersection(gene_set))
+        census[species] = {'ens': n_ens, 'symb': n_symb}
+
+    # check presence of EnsemblIDs
+    chosen_species = None
+    error_msg = ""
+    for species in census:
+        if census[species]['ens'] == 0:
+            continue
+        if chosen_species is not None:
+            error_msg += (
+                f"There are EnsemblIDs from {chosen_species} and {species}\n")
+        chosen_species = species
+    if len(error_msg) > 0:
+        raise RuntimeError(
+            f"{error_msg}Unclear how to choose species.")
+    if chosen_species is not None:
+        return chosen_species
+
+    n_max = 0
+    chosen_species = []
+    for species in census:
+        if census[species]['symb'] > n_max:
+            chosen_species = [species]
+            n_max = census[species]['symb']
+        elif census[species]['symb'] == n_max and n_max > 0:
+            chosen_species.append(species)
+
+    if len(chosen_species) > 1:
+        error_msg = (
+            f"These species\n{chosen_species}\nall have {n_max} gene symbols "
+            "present in data. Unclear how to choose species."
+        )
+        raise RuntimeError(error_msg)
+    if len(chosen_species) == 0:
+        return None
+    return chosen_species[0]
