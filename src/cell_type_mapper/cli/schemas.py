79741f7e3e79b276fcca54a70c2774b990fb2a44
danielsf <scott.daniel@alleninstitute.org>
2023-10-27 10:14:40 -0700

    start consolidating schemaswq

--- src/cell_type_mapper/cli/schemas.py
+++ src/cell_type_mapper/cli/schemas.py
@@ -1,8 +1,11 @@
 import argschema
 from marshmallow import post_load, ValidationError
 
+from cell_type_mapper.utils.anndata_utils import (
+    does_obsm_have_key)
 
-class SpecifiedPrecomputedStatsSchema(argschema.ArgSchema):
+
+class PrecomputedStatsInputSchema(argschema.ArgSchema):
 
     path = argschema.fields.InputFile(
                 required=True,
@@ -14,7 +17,7 @@ class SpecifiedPrecomputedStatsSchema(argschema.ArgSchema):
                 "stats file for this mapping job.")
 
 
-class SpecifiedMarkerSchema(argschema.ArgSchema):
+class QueryMarkerInputSchema(argschema.ArgSchema):
 
     serialized_lookup = argschema.fields.InputFile(
         required=True,
@@ -95,5 +98,145 @@ class HierarchicalTypeAssignmentSchema(argschema.ArgSchema):
             raise ValidationError(
                 f"{norm} is not a valid query normalization;\n"
                 "must be either 'raw' or 'log2CP'")
+
+        return data
+
+
+class FromSpecifiedMarkersSchema(argschema.ArgSchema):
+
+    tmp_dir = argschema.fields.OutputDir(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="Optional temporary directory into which data "
+        "will be copied for faster access (e.g. if the data "
+        "naturally lives on a slow NFS drive)")
+
+    query_path = argschema.fields.InputFile(
+        required=True,
+        default=None,
+        allow_none=False,
+        description="Path to the h5ad file containing the query "
+        "dataset")
+
+    map_to_ensembl = argschema.fields.Boolean(
+        reauired=False,
+        default=False,
+        allow_none=False,
+        description="If True, map the gene names in query_path to "
+        "ENSEMBL IDs before performing cell type mapping.")
+
+    extended_result_dir = argschema.fields.OutputDir(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="Optional temporary directory into which assignment "
+        "results will be saved from each process.")
+
+    extended_result_path = argschema.fields.OutputFile(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="Path to JSON file where extended results "
+        "will be saved.")
+
+    obsm_key = argschema.fields.String(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="If not None, save the results of the "
+        "mapping in query_path.obsm under this key")
+
+    obsm_clobber = argschema.fields.Boolean(
+        required=False,
+        default=False,
+        allow_none=False,
+        description="If True, allow the code to overwrite an "
+        "existing element in query_path.obsm")
+
+    csv_result_path = argschema.fields.OutputFile(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="Path to CSV file where output file will be "
+        "written (if None, no CSV will be produced).")
+
+    log_path = argschema.fields.String(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="Path to the log file to be written")
+
+    max_gb = argschema.fields.Float(
+        required=False,
+        default=100.0,
+        allow_none=False,
+        description="In the event that a CSC matrix needs to be "
+        "converted to a temporary on disk CSR matrix, how "
+        "much memory (in gigabytes) can we use.")
+
+    drop_level = argschema.fields.String(
+        required=False,
+        default=None,
+        allow_none=True,
+        description="If this level exists in the taxonomy, drop "
+        "it before doing type assignment (this is to accommmodate "
+        "the fact that the official taxonomy includes the "
+        "'supertype', even though that level is not used "
+        "during hierarchical type assignment")
+
+    precomputed_stats = argschema.fields.Nested(
+        PrecomputedStatsInputSchema,
+        required=True)
+
+    query_markers = argschema.fields.Nested(
+        QueryMarkerInputSchema,
+        required=True)
+
+    type_assignment = argschema.fields.Nested(
+        HierarchicalTypeAssignmentSchema,
+        required=True)
+
+    flatten = argschema.fields.Boolean(
+        required=False,
+        default=False,
+        allow_none=False,
+        description="If true, flatten the taxonomy so that we are "
+        "mapping directly to the leaf node")
+
+    cloud_safe = argschema.fields.Boolean(
+        required=False,
+        default=True,
+        allow_nonw=False,
+        description="If True, full file paths not recorded in log")
+
+    @post_load
+    def check_result_dst(self, data, **kwargs):
+        """
+        Make sure that there is somewhere, either extended_result_path
+        or obsm_key, where we can store the extended results.
+        """
+        if data['extended_result_path'] is None:
+            if data['obsm_key'] is None:
+                msg = ("You must specify at least one of extended_result_path "
+                       "and/or obsm_key")
+                raise RuntimeError(msg)
+        return data
+
+    @post_load
+    def check_obsm_key(self, data, **kwargs):
+        """
+        If obsm_key is not None, make sure that key has not already
+        been assigned in query_path.obsm
+        """
+        if data['obsm_key'] is None:
+            return data
+
+        if does_obsm_have_key(data['query_path'], data['obsm_key']):
+            if not data['obsm_clobber']:
+                msg = (f"obsm in {data['query_path']} already has key "
+                       f"{data['obsm_key']}; to overwrite, set obsm_clobber "
+                       "to True.")
+                raise RuntimeError(msg)
 
         return data
