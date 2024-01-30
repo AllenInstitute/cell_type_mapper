827c594be37e0870951425c938e078160525d693
danielsf <scott.daniel@alleninstitute.org>
2024-01-30 12:34:40 -0800

    start testing marker finder worker

--- tests/reference_markers/test_p_mask_markers.py
+++ tests/reference_markers/test_p_mask_markers.py
@@ -19,76 +19,6 @@ def tmp_dir(tmp_path_factory):
     _clean_up(tmp_dir)
 
 
-@pytest.fixture(scope='module')
-def gene_names():
-    """
-    List of gene names from this dataset.
-    """
-    return [f'g_{ii}' for ii in range(100)]
-
-
-@pytest.fixture(scope='module')
-def _pair_to_idx():
-    """
-    The pair_to_idx dict that needs to be in the p_mask
-    and reference marker HDF5 files.
-    """
-    clusters = [f'cl_{ii}' for ii in range(5)]
-    ct = 0
-    result = dict()
-    result['cluster'] = dict()
-    pair_list = []
-    for pair in itertools.combinations(clusters, 2):
-        pair = list(pair)
-        pair.sort()
-        pair_list.append(pair)
-        if pair[0] not in result['cluster']:
-            result['cluster'][pair[0]] = dict()
-        result['cluster'][pair[0]][pair[1]] = ct
-        ct += 1
-    return result, ct, pair_list
-
-
-@pytest.fixture(scope='module')
-def pair_to_idx(_pair_to_idx):
-    """
-    The pair_to_idx dict that needs to be in the p_mask
-    and reference marker HDF5 files.
-    """
-    return _pair_to_idx[0]
-
-
-@pytest.fixture(scope='module')
-def n_pairs(_pair_to_idx):
-    return _pair_to_idx[1]
-
-
-@pytest.fixture(scope='module')
-def pair_list(_pair_to_idx):
-    return _pair_to_idx[2]
-
-
-@pytest.fixture(scope='module')
-def pair_to_radius(
-        pair_list,
-        pair_to_idx,
-        gene_names):
-    """
-    A dict mapping a cluster_pair to a lookup table
-    mapping gene_name to penetrance radius.
-    """
-    rng = np.random.default_rng(221312)
-    for pair in pair_list:
-        pair_idx = pair_to_idx['cluster'][pair[0]][pair[1]]
-        radii = np.zeros(len(gene_names))
-
-        # genes that are 'absolutely valid'
-        i0 = 3*(pair_idx)
-        i1 = 3*(pair_idx+1)
-        radii[i0:i1] = -1.0
-
-
-
 @pytest.mark.parametrize(
     "n_valid, valid_gene_idx, expected_markers",
     [(3, None, (6, 9, 11)),  # genes that absolutely pass penetrance test
@@ -126,11 +56,3 @@ def test_get_validity_mask(
         expected[ii] = True
 
     np.testing.assert_array_equal(expected, actual)
-
-
-
-def test_dummy(
-        pair_list,
-        n_pairs,
-        pair_to_idx):
-    pass
