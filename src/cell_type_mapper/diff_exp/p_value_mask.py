3f023b342b0dae746c611b413ef5732a5df24f12
danielsf <scott.daniel@alleninstitute.org>
2024-01-31 08:28:19 -0800

    adjust n_per in p value mask calculation

--- src/cell_type_mapper/diff_exp/p_value_mask.py
+++ src/cell_type_mapper/diff_exp/p_value_mask.py
@@ -135,7 +135,7 @@ def _create_p_value_mask_file(
     n_pairs = len(idx_to_pair)
 
     # how many pairs to run per proceess
-    n_per = min(40000, n_pairs//(2*n_processors))
+    n_per = min(10000, n_pairs//(2*n_processors))
     n_per -= (n_per % 8)
     n_per = max(8, n_per)
     t0 = time.time()
