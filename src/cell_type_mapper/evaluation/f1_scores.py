f3646c6edd15c9ca30fc76d4988a6e4cffcaeff3
danielsf <scott.daniel@alleninstitute.org>
2024-05-10 11:53:25 -0700

    further tweaking of F1 function

--- src/cell_type_mapper/evaluation/f1_scores.py
+++ src/cell_type_mapper/evaluation/f1_scores.py
@@ -46,6 +46,7 @@ def avg_f1(
     true_pos = dict()
     false_pos = dict()
     false_neg = dict()
+    true_neg = dict()
     n_cells = dict()
     n_tot = dict()
     for level in taxonomy_tree.hierarchy:
@@ -53,30 +54,48 @@ def avg_f1(
         true_pos[level] = np.zeros(n_nodes, dtype=int)
         false_pos[level] = np.zeros(n_nodes, dtype=int)
         false_neg[level] = np.zeros(n_nodes, dtype=int)
+        true_neg[level] = np.zeros(n_nodes, dtype=int)
         n_cells[level] = np.zeros(n_nodes, dtype=int)
         n_tot[level] = 0
 
-    n_skipped = 0
+    estimated_false_pos = {
+        level:0 for level in taxonomy_tree.hierarchy
+    }
     for cell in mapping:
         for level in taxonomy_tree.hierarchy:
-            if probability_cut is not None:
-                if 'aggregate_probability' in cell[level]:
-                    actual = cell[level]['aggregate_probability']
-                    if actual < probability_cut:
-                        n_skipped += 1
-                        continue
-
             assigned_val = cell[level]['assignment']
             true_val = truth[cell['cell_id']][level]
             true_idx = nodes_to_idx[level][true_val]
             assigned_idx = nodes_to_idx[level][assigned_val]
+            if assigned_idx == true_idx:
+                is_true = True
+            else:
+                is_true = False
+
+            agg_prob = None
+            considered_true = True
+            if probability_cut is not None:
+                if 'aggregate_probability' in cell[level]:
+                    agg_prob = cell[level]['aggregate_probability']
+                    if agg_prob < probability_cut:
+                        considered_true=False
+
             n_cells[level][true_idx] += 1
             n_tot[level] += 1
-            if assigned_val == true_val:
-                true_pos[level][true_idx] += 1
+
+            if considered_true:
+                if agg_prob is not None:
+                    estimated_false_pos[level] += (1.0-agg_prob)
+
+            if is_true:
+                if considered_true:
+                    true_pos[level][true_idx] += 1
+                else:
+                    false_neg[level][true_idx] += 1
             else:
                 false_neg[level][true_idx] += 1
-                false_pos[level][assigned_idx] += 1
+                if considered_true:
+                    false_pos[level][assigned_idx] += 1
 
     results = dict()
     for level in taxonomy_tree.hierarchy:
@@ -90,7 +109,12 @@ def avg_f1(
         results[level]['macro'] = np.nanmean(f1_vals)
         adj = np.where(np.isfinite(f1_vals), f1_vals, 0.0)
         results[level]['macro_adjusted'] = np.mean(adj)
+        results[level]['true_pos'] = int(true_pos[level].sum())
+        results[level]['true_neg'] = int(true_neg[level].sum())
+        results[level]['false_pos'] = int(false_pos[level].sum())
+        results[level]['false_neg'] = int(false_neg[level].sum())
         results[level]['n_cells'] = n_tot[level]
         results[level]['valid_classes'] = int(np.isfinite(f1_vals).sum())
+        results[level]['est_false_pos'] = estimated_false_pos[level]
 
     return results
