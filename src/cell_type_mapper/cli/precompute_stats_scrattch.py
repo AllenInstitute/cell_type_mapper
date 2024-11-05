23c6d0efb20213bb9bef210a6b317403bc0ae5ac
danielsf <scott.daniel@alleninstitute.org>
2024-11-05 13:22:45 -0800

    scrattch precompute CLI correctly records useful config

--- src/cell_type_mapper/cli/precompute_stats_scrattch.py
+++ src/cell_type_mapper/cli/precompute_stats_scrattch.py
@@ -4,13 +4,16 @@ from a scrattch-compliant h5ad file.
 """
 
 import argschema
-import copy
 import h5py
 import json
 import time
 
 from cell_type_mapper.utils.output_utils import (
     get_execution_metadata)
+
+from cell_type_mapper.utils.cli_utils import (
+    config_from_args
+)
 
 from cell_type_mapper.diff_exp.precompute_from_anndata import (
     precompute_summary_stats_from_h5ad)
@@ -25,6 +28,13 @@ class PrecomputationScrattchRunner(argschema.ArgSchemaParser):
 
     def run(self):
         t0 = time.time()
+
+        metadata = {
+            'config': config_from_args(
+                        input_config=self.args,
+                        cloud_safe=False)
+        }
+
         precompute_summary_stats_from_h5ad(
             data_path=self.args['h5ad_path'],
             column_hierarchy=self.args['hierarchy'],
@@ -35,10 +45,6 @@ class PrecomputationScrattchRunner(argschema.ArgSchemaParser):
             tmp_dir=self.args['tmp_dir'],
             n_processors=self.args['n_processors'],
             layer=self.args['layer'])
-
-        metadata = {
-            'config': copy.deepcopy(self.args)
-        }
 
         metadata.update(
             get_execution_metadata(
