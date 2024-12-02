ddb2173690099f82352c805b222e0aff48c0177e
danielsf <scott.daniel@alleninstitute.org>
2024-12-02 12:59:23 -0800

    try to speed test up by caching fixtures

--- tests/integration/test_specified_markers_smoke.py
+++ tests/integration/test_specified_markers_smoke.py
@@ -161,52 +161,71 @@ def query_h5ad_fixture(
         tmp_dir_fixture,
         n_extra_genes_fixture):
 
-    h5ad_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='query_data_',
-        suffix='.h5ad')
+    return create_query_h5ad(
+        density_specification=density_fixture,
+        gene_name_list=gene_name_fixture,
+        tmp_dir_path=tmp_dir_fixture,
+        n_extra_genes=n_extra_genes_fixture
+    )
 
-    n_cells = 321
-    n_genes = len(gene_name_fixture) + n_extra_genes_fixture
 
-    rng = np.random.default_rng(77123)
+def create_query_h5ad(
+        density_specification,
+        gene_name_list,
+        tmp_dir_path,
+        n_extra_genes):
 
-    if density_fixture == 'dense':
-        X = rng.random((n_cells, n_genes), dtype=np.float32)
-    else:
-        n_tot = n_cells*n_genes
-        data = np.zeros(n_tot, dtype=int)
-        chosen_idx = rng.choice(n_tot, n_tot//3, replace=False)
-        data[chosen_idx] = rng.integers(1, 255, len(chosen_idx))
-        data = data.reshape((n_cells, n_genes))
-        if density_fixture == 'csc':
-            X = scipy.sparse.csc_matrix(data)
-        elif density_fixture == 'csr':
-            X = scipy.sparse.csr_matrix(data)
+    if not hasattr(create_query_h5ad, '_cache'):
+        create_query_h5ad._cache = dict()
+
+    if density_specification not in create_query_h5ad._cache:
+        h5ad_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='query_data_',
+            suffix='.h5ad')
+
+        n_cells = 321
+        n_genes = len(gene_name_list) + n_extra_genes
+
+        rng = np.random.default_rng(77123)
+
+        if density_specification == 'dense':
+            X = rng.random((n_cells, n_genes), dtype=np.float32)
         else:
-            raise RuntimeError(
-                f'cannot parse density {density_fixture}'
-            )
+            n_tot = n_cells*n_genes
+            data = np.zeros(n_tot, dtype=int)
+            chosen_idx = rng.choice(n_tot, n_tot//3, replace=False)
+            data[chosen_idx] = rng.integers(1, 255, len(chosen_idx))
+            data = data.reshape((n_cells, n_genes))
+            if density_specification == 'csc':
+                X = scipy.sparse.csc_matrix(data)
+            elif density_specification == 'csr':
+                X = scipy.sparse.csr_matrix(data)
+            else:
+                raise RuntimeError(
+                    f'cannot parse density {density_specification}'
+                )
 
-    obs = pd.DataFrame(
-        [{'cell_id': f'cell_{ii}'}
-         for ii in range(n_cells)]).set_index('cell_id')
+        obs = pd.DataFrame(
+            [{'cell_id': f'cell_{ii}'}
+             for ii in range(n_cells)]).set_index('cell_id')
 
-    these_gene_names = copy.deepcopy(gene_name_fixture)
-    for ii in range(n_extra_genes_fixture):
-        these_gene_names.append(f'extra_gene_{ii}')
-    rng.shuffle(these_gene_names)
+        these_gene_names = copy.deepcopy(gene_name_list)
+        for ii in range(n_extra_genes):
+            these_gene_names.append(f'extra_gene_{ii}')
+        rng.shuffle(these_gene_names)
 
-    var = pd.DataFrame(
-        [{'gene_name': g} for g in these_gene_names]).set_index('gene_name')
+        var = pd.DataFrame(
+            [{'gene_name': g} for g in these_gene_names]).set_index('gene_name')
 
-    a_data = anndata.AnnData(
-        X=X,
-        obs=obs,
-        var=var)
+        a_data = anndata.AnnData(
+            X=X,
+            obs=obs,
+            var=var)
 
-    a_data.write_h5ad(h5ad_path)
-    return h5ad_path
+        a_data.write_h5ad(h5ad_path)
+        create_query_h5ad._cache[density_specification] = h5ad_path
+    return create_query_h5ad._cache[density_specification]
 
 
 @pytest.fixture()
@@ -214,81 +233,104 @@ def reference_mapping_fixture(
         query_h5ad_fixture,
         marker_lookup_fixture,
         precomputed_stats_fixture,
+        density_fixture,
         tmp_dir_fixture):
     """
     A fixture returning what the mapping should be
     (for comparing results when we are mapping data
     that lacked the encoding-type metadata field)
     """
+    return do_reference_mapping(
+        query_h5ad_path=query_h5ad_fixture,
+        marker_lookup_path=marker_lookup_fixture,
+        precomputed_stats_path=precomputed_stats_fixture,
+        density_specification=density_fixture,
+        tmp_dir_path=tmp_dir_fixture
+    )
 
-    validated_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='validated_',
-        suffix='.h5ad')
 
-    output_json_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='output_',
-        suffix='.json')
+def do_reference_mapping(
+        query_h5ad_path,
+        marker_lookup_path,
+        precomputed_stats_path,
+        density_specification,
+        tmp_dir_path):
 
-    validation_config = {
-        'h5ad_path': str(query_h5ad_fixture),
-        'valid_h5ad_path': validated_path,
-        'output_json': output_json_path}
+    if not hasattr(do_reference_mapping, '_cache'):
+        do_reference_mapping._cache = dict()
 
-    runner = ValidateH5adRunner(
-        args=[],
-        input_data=validation_config)
-    runner.run()
+    if density_specification not in do_reference_mapping._cache:
+        validated_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='validated_',
+            suffix='.h5ad')
 
-    output_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='outptut_',
-        suffix='.json')
+        output_json_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='output_',
+            suffix='.json')
 
-    csv_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='csv_output_',
-        suffix='.csv')
+        validation_config = {
+            'h5ad_path': str(query_h5ad_path),
+            'valid_h5ad_path': validated_path,
+            'output_json': output_json_path}
 
-    metadata_path = mkstemp_clean(
-        dir=tmp_dir_fixture,
-        prefix='summary_',
-        suffix='.json')
+        runner = ValidateH5adRunner(
+            args=[],
+            input_data=validation_config)
+        runner.run()
 
-    config = {
-        'precomputed_stats': {
-            'path': str(precomputed_stats_fixture)
-        },
-        'query_markers': {
-            'serialized_lookup': str(marker_lookup_fixture)
-        },
-        'query_path': validated_path,
-        'extended_result_path': str(output_path),
-        'csv_result_path': str(csv_path),
-        'summary_metadata_path': metadata_path,
-        'map_to_ensembl': False,
-        'type_assignment': {
-            'normalization': 'log2CPM',
-            'bootstrap_iteration': 10,
-            'bootstrap_factor': 0.9,
-            'n_runners_up': 2,
-            'rng_seed': 5513,
-            'chunk_size': 50,
-            'n_processors': 3
+        output_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='outptut_',
+            suffix='.json')
+
+        csv_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='csv_output_',
+            suffix='.csv')
+
+        metadata_path = mkstemp_clean(
+            dir=tmp_dir_path,
+            prefix='summary_',
+            suffix='.json')
+
+        config = {
+            'precomputed_stats': {
+                'path': str(precomputed_stats_path)
+            },
+            'query_markers': {
+                'serialized_lookup': str(marker_lookup_path)
+            },
+            'query_path': validated_path,
+            'extended_result_path': str(output_path),
+            'csv_result_path': str(csv_path),
+            'summary_metadata_path': metadata_path,
+            'map_to_ensembl': False,
+            'type_assignment': {
+                'normalization': 'log2CPM',
+                'bootstrap_iteration': 10,
+                'bootstrap_factor': 0.9,
+                'n_runners_up': 2,
+                'rng_seed': 5513,
+                'chunk_size': 50,
+                'n_processors': 3
+            }
         }
-    }
 
-    runner = FromSpecifiedMarkersRunner(
-        args=[],
-        input_data=config)
+        runner = FromSpecifiedMarkersRunner(
+            args=[],
+            input_data=config)
 
-    runner.run()
+        runner.run()
 
-    return {
-        'csv_path': csv_path,
-        'json_path': output_path
-    }
+        this = {
+            'csv_path': csv_path,
+            'json_path': output_path
+        }
+        do_reference_mapping._cache[density_specification] = this
+
+    return do_reference_mapping._cache[density_specification]
 
 
 @pytest.mark.parametrize(
@@ -393,19 +435,20 @@ def test_online_workflow_WMB(
 
     runner.run()
 
-    test_df = pd.read_csv(
-        csv_path,
-        comment='#')
+    if not with_encoding_type:
+        test_df = pd.read_csv(
+            csv_path,
+            comment='#')
 
-    baseline_df = pd.read_csv(
-        reference_mapping_fixture['csv_path'],
-        comment='#')
+        baseline_df = pd.read_csv(
+            reference_mapping_fixture['csv_path'],
+            comment='#')
 
-    pd.testing.assert_frame_equal(test_df, baseline_df)
+        pd.testing.assert_frame_equal(test_df, baseline_df)
 
-    test_dict = json.load(open(output_path, 'rb'))
-    baseline_dict = json.load(open(reference_mapping_fixture['json_path'], 'rb'))
-    assert test_dict['results'] == baseline_dict['results']
+        test_dict = json.load(open(output_path, 'rb'))
+        baseline_dict = json.load(open(reference_mapping_fixture['json_path'], 'rb'))
+        assert test_dict['results'] == baseline_dict['results']
 
 
 @pytest.mark.parametrize('map_to_ensembl,write_summary',
