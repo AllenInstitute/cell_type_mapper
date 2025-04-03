0afebd467dc9b77f41604231d9aaf1ddc2abd164
danielsf <scott.daniel@alleninstitute.org>
2025-04-03 00:04:53 -0700

    add tests for CLI validation of CSV files

--- tests/validation/conftest.py
+++ tests/validation/conftest.py
@@ -7,6 +7,10 @@ from cell_type_mapper.utils.utils import (
     mkstemp_clean
 )
 
+from cell_type_mapper.data.mouse_gene_id_lookup import (
+    mouse_gene_id_lookup
+)
+
 
 @pytest.fixture()
 def suffix_fixture(request):
@@ -24,11 +28,42 @@ def label_type_fixture(request):
 
 
 @pytest.fixture()
+def gene_identifier_type_fixture(request):
+    if not hasattr(request, 'param'):
+        return 'ensembl'
+    return request.param
+
+
+@pytest.fixture()
+def x_dtype_fixture(request):
+    if not hasattr(request, 'param'):
+        return 'integer'
+    return request.param
+
+@pytest.fixture()
+def transposition_fixture(request):
+    if not hasattr(request, 'param'):
+        return False
+    assert isinstance(request.param, bool)
+    return request.param
+
+
+@pytest.fixture()
 def csv_anndata_fixture(
         suffix_fixture,
         label_heading_fixture,
         label_type_fixture,
+        gene_identifier_type_fixture,
+        x_dtype_fixture,
+        transposition_fixture,
         tmp_dir_fixture):
+    """
+    Returns
+        path to CSV file
+        list of cell_labels
+        list of gene_labels
+        x matrix as an array
+    """
 
     suffix = suffix_fixture
     label_heading = label_heading_fixture
@@ -43,13 +78,28 @@ def csv_anndata_fixture(
         'random',
         'sequential_float',
         'big_float',
-        'hybrid')
+        'hybrid',
+        'degenerate')
 
     rng = np.random.default_rng(221111)
     n_cells = 4
     n_genes = 7
-    x = rng.integers(10, 100, (n_cells, n_genes))
-    gene_labels = [f'g_{ii}' for ii in range(n_genes)]
+    if x_dtype_fixture == 'integer':
+        x = rng.integers(10, 100, (n_cells, n_genes))
+    elif x_dtype_fixture == 'float':
+        x = 10.0*rng.random((n_cells, n_genes))
+    else:
+        raise RuntimeError(
+            "unclear what type of X you want -- "
+            f"{x_dtype_fixture}")
+
+    if gene_identifier_type_fixture == 'ensembl':
+        gene_labels = sorted(set(mouse_gene_id_lookup.values()))[:n_genes]
+    elif gene_identifier_type_fixture == 'symbol':
+        gene_labels = sorted(set(mouse_gene_id_lookup.keys()))[:n_genes]
+    else:
+        raise RuntimeError("unclear what gene identifiers you want")
+
     if label_type == 'sequential':
         cell_labels = [ii for ii in range(n_cells)]
     elif label_type == 'string':
@@ -72,6 +122,10 @@ def csv_anndata_fixture(
         cell_labels = [
             11, 'aa', 3, 2
         ]
+    elif label_type == 'degenerate':
+        cell_labels = [
+            'cellA', 'cellA', 'cellB', 'cellB'
+        ]
 
     csv_path = mkstemp_clean(
         dir=tmp_dir_fixture,
@@ -85,17 +139,29 @@ def csv_anndata_fixture(
         open_fn = gzip.open
         is_gzip = True
 
+    if transposition_fixture:
+        row_labels = gene_labels
+        col_labels = cell_labels
+        output_x = x.transpose()
+    else:
+        row_labels = cell_labels
+        col_labels = gene_labels
+        output_x = x
+
     with open_fn(csv_path, 'w') as dst:
         data = ''
         if label_heading:
             data += 'cell_label'
-        for label in gene_labels:
+        for label in col_labels:
             data += f',{label}'
         data += '\n'
-        for i_row in range(n_cells):
-            data += f'{cell_labels[i_row]}'
-            for i_col in range(n_genes):
-                data += f',{x[i_row, i_col]}'
+        for i_row in range(len(row_labels)):
+            data += f'{row_labels[i_row]}'
+            for i_col in range(len(col_labels)):
+                if x_dtype_fixture == 'integer':
+                    data += f',{output_x[i_row, i_col]}'
+                else:
+                    data += f',{output_x[i_row, i_col]:.4e}'
             data += '\n'
         if is_gzip:
             data = data.encode('utf-8')
---
+++ tests/validation/test_csv_cli_validation.py
@@ -0,0 +1,134 @@
+"""
+Unit tests for passing a CSV file through the validator
+"""
+import pytest
+
+import anndata
+import hashlib
+import itertools
+import json
+import numpy as np
+import warnings
+
+from cell_type_mapper.utils.utils import(
+    mkstemp_clean
+)
+
+from cell_type_mapper.data.mouse_gene_id_lookup import (
+    mouse_gene_id_lookup
+)
+
+from cell_type_mapper.cli.validate_h5ad import (
+    ValidateH5adRunner
+)
+
+
+@pytest.mark.parametrize(
+    "label_heading_fixture,label_type_fixture,suffix_fixture,"
+    "gene_identifier_type_fixture,x_dtype_fixture,transposition_fixture",
+    itertools.product(
+        [True, False],
+        ["big", "degenerate", "sequential", "big", "random", "string"],
+        [".csv", ".csv.gz"],
+        ["ensembl", "symbol"],
+        ["integer", "float"],
+        [True, False]
+    ),
+    indirect=[
+        "label_heading_fixture",
+        "label_type_fixture",
+        "suffix_fixture",
+        "gene_identifier_type_fixture",
+        "x_dtype_fixture",
+        "transposition_fixture"
+    ]
+)
+def test_cli_validation_for_csv(
+        label_heading_fixture,
+        label_type_fixture,
+        suffix_fixture,
+        gene_identifier_type_fixture,
+        x_dtype_fixture,
+        transposition_fixture,
+        csv_anndata_fixture,
+        tmp_dir_fixture):
+
+    if label_heading_fixture:
+        if label_type_fixture == "random":
+            return
+
+    if transposition_fixture:
+        if label_type_fixture not in ("string", "degenerate"):
+            return
+
+    (csv_path,
+     cell_labels,
+     gene_labels,
+     x_array) = csv_anndata_fixture
+
+    md50 = hashlib.md5()
+    with open(csv_path, 'rb') as src:
+        md50.update(src.read())
+
+    json_path = mkstemp_clean(
+        dir=tmp_dir_fixture,
+        suffix='.json'
+    )
+
+    dst_path = mkstemp_clean(
+        dir=tmp_dir_fixture,
+        suffix='.h5ad'
+    )
+
+    config = {
+        'input_path': csv_path,
+        'output_json': json_path,
+        'valid_h5ad_path': dst_path
+    }
+
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        runner = ValidateH5adRunner(args=[], input_data=config)
+        runner.run()
+
+    md51 = hashlib.md5()
+    with open(csv_path, 'rb') as src:
+        md51.update(src.read())
+    assert md50.hexdigest() == md51.hexdigest()
+
+    result = anndata.read_h5ad(dst_path, backed='r')
+    if x_dtype_fixture == 'integer':
+        np.testing.assert_array_equal(
+            result.X[()],
+            x_array
+        )
+    else:
+        np.testing.assert_array_equal(
+            result.X[()],
+            np.round(x_array).astype(int)
+        )
+
+    if label_type_fixture != 'degenerate':
+        expected_cell_labels = np.array(cell_labels).astype(str)
+    else:
+        expected_cell_labels = np.array(
+            [json.dumps({"original_index": label, "row": ii})
+             for ii, label in enumerate(cell_labels)]
+        )
+
+    np.testing.assert_array_equal(
+        result.obs.index.values,
+        expected_cell_labels
+    )
+
+    if gene_identifier_type_fixture == 'ensembl':
+        expected_gene_labels = np.array(gene_labels)
+    else:
+        expected_gene_labels = np.array(
+            [mouse_gene_id_lookup[l] for l in gene_labels]
+        )
+
+    np.testing.assert_array_equal(
+        result.var.index.values,
+        expected_gene_labels
+    )
