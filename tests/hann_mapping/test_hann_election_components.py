cfaac4b101962ec03a8b3e762f69aed65e54f59d
danielsf <scott.daniel@alleninstitute.org>
2025-09-26 11:46:18 -0700

    add function to collate hann mapping results
    move output collation to finally: block
    
    should make it easier to support hann algorithm

--- tests/hann_mapping/test_hann_election_components.py
+++ tests/hann_mapping/test_hann_election_components.py
@@ -445,6 +445,16 @@ def test_hann_tally_votes_smoke(
     assert col_sum.shape == (reference.n_cells, )
     assert col_sum.min() > 0
 
+    np.testing.assert_array_equal(
+        actual=np.array(result['cluster_identifiers']),
+        desired=np.array(reference.cell_identifiers)
+    )
+
+    np.testing.assert_array_equal(
+        actual=np.array(result['cell_identifiers']),
+        desired=np.array(query.cell_identifiers)
+    )
+
 
 def test_hann_mapping_chunk_smoke(
         tmp_dir_fixture,
@@ -511,6 +521,7 @@ def test_hann_mapping_chunk_smoke(
         votes = src["votes"][()]
         corr = src["correlation"][()]
         cell_id = src["cell_identifiers"][()]
+        cluster_id = src["cluster_identifiers"][()]
 
     assert votes.sum() == bootstrap_iteration*query.n_cells
     # make sure there is a diversity of vote counts
@@ -525,4 +536,124 @@ def test_hann_mapping_chunk_smoke(
     np.testing.assert_array_equal(
         desired=query.cell_identifiers,
         actual=[c.decode('utf-8') for c in cell_id]
+    )
+
+    np.testing.assert_array_equal(
+        desired=reference.cell_identifiers,
+        actual=[c.decode('utf-8') for c in cluster_id]
+    )
+
+
+def test_collate_hann_mappings(
+        tmp_dir_fixture):
+
+    rng = np.random.default_rng(21311)
+    n_cells = 150
+    n_clusters = 7
+    expected_cell_id = np.array(
+        [f'c{ii}'.encode('utf-8') for ii in range(n_cells)]
+    )
+    expected_votes = rng.integers(0, 256, (n_cells, n_clusters))
+    expected_corr = rng.random((n_cells, n_clusters))
+    expected_cluster_id = np.array(
+        [f'cl_{ii}'.encode('utf-8') for ii in range(n_clusters)]
+    )
+
+    path_list = [
+        ctm_utils.mkstemp_clean(
+            dir=tmp_dir_fixture,
+            suffix='.h5'
+        )
+        for ii in range(3)
+    ]
+    with h5py.File(path_list[0], 'w') as dst:
+        dst.create_dataset(
+            'cell_identifiers',
+            data=expected_cell_id[:54]
+        )
+        dst.create_dataset(
+            'votes',
+            data=expected_votes[:54, :]
+        )
+        dst.create_dataset(
+            'correlation',
+            data=expected_corr[:54, :]
+        )
+        dst.create_dataset(
+            'cluster_identifiers',
+            data=expected_cluster_id
+        )
+
+    with h5py.File(path_list[1], 'w') as dst:
+        dst.create_dataset(
+            'cell_identifiers',
+            data=expected_cell_id[54:91]
+        )
+        dst.create_dataset(
+            'votes',
+            data=expected_votes[54:91, :]
+        )
+        dst.create_dataset(
+            'correlation',
+            data=expected_corr[54:91, :]
+        )
+        dst.create_dataset(
+            'cluster_identifiers',
+            data=expected_cluster_id
+        )
+
+    with h5py.File(path_list[2], 'w') as dst:
+        dst.create_dataset(
+            'cell_identifiers',
+            data=expected_cell_id[91:]
+        )
+        dst.create_dataset(
+            'votes',
+            data=expected_votes[91:, :]
+        )
+        dst.create_dataset(
+            'correlation',
+            data=expected_corr[91:, :]
+        )
+        dst.create_dataset(
+            'cluster_identifiers',
+            data=expected_cluster_id
+        )
+
+    dst_path = ctm_utils.mkstemp_clean(
+        dir=tmp_dir_fixture,
+        prefix='collated_hann_mapping_',
+        suffix='.h5'
+    )
+    hann_mapping.collate_hann_mappings(
+        tmp_path_list=path_list,
+        dst_path=dst_path
+    )
+
+    with h5py.File(dst_path, 'r') as src:
+        cell_id = src['cell_identifiers'][()]
+        cluster_id = src['cluster_identifiers'][()]
+        votes = src['votes'][()]
+        correlation = src['correlation'][()]
+
+    np.testing.assert_array_equal(
+        actual=cell_id,
+        desired=expected_cell_id
+    )
+
+    np.testing.assert_array_equal(
+        actual=cluster_id,
+        desired=expected_cluster_id
+    )
+
+    np.testing.assert_array_equal(
+        actual=votes,
+        desired=expected_votes
+    )
+
+    np.testing.assert_allclose(
+        actual=correlation,
+        desired=expected_corr,
+        atol=0.0,
+        rtol=1.0e-6
     )
